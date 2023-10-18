071e3ca5e2108a4332addcdae645e579e0b8e863
CCCWS <eung0626@gmail.com>
2023-10-18 23:41:52 +0900

    weather

--- src/WeatherPage/WeatherChart.tsx
+++ src/WeatherPage/WeatherChart.tsx
@@ -1,18 +1,34 @@
 import React from "react";
 import styled from "styled-components";
 
+interface tempType {
+  temp: number;
+  x1: string;
+  x2: string;
+  y1: string;
+  y2: string;
+}
+
 const WeatherChart = () => {
   const test = { color: "blue" };
+  const temp: tempType[] = [
+    { temp: 9, x1: "0", x2: "50", y1: "0", y2: "9%" },
+    { temp: 15, x1: "50", x2: "100", y1: "9%", y2: "15%" },
+    { temp: 1, x1: "100", x2: "150", y1: "15%", y2: "1%" },
+    { temp: 10, x1: "150", x2: "200", y1: "1%", y2: "10%" },
+  ];
   return (
     <Div>
       <Svg>
-        <line
-          x1="0"
-          x2="300"
-          y1="0"
-          y2="300"
-          style={{ stroke: "rgb(255,0,0)", strokeWidth: 2 }}
-        ></line>
+        {temp.map((data, index) => (
+          <Line
+            x1={data.x1}
+            x2={data.x2}
+            y1={data.y1}
+            y2={data.y2}
+            key={index}
+          ></Line>
+        ))}
       </Svg>
     </Div>
   );
@@ -27,8 +43,17 @@ const Div = styled.div`
 const Svg = styled.svg`
   width: 100%;
   height: 100%;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 
-// const Line = styled.line``;
+const Line = styled.line`
+  stroke: rgb(255, 0, 0);
+  stroke-width: 3;
+  
+`;
 
 export default WeatherChart;
