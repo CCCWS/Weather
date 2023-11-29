97bca64942749e797304fb520a7cf385a200929a
CCCWS <eung0626@gmail.com>
2023-11-29 22:18:53 +0900

    weather

--- src/ImageGallery/Test_3.tsx
+++ src/ImageGallery/Test_3.tsx
@@ -31,67 +31,107 @@ const Test_3 = () => {
       console.log(answer);
     };
 
-    compression(word);
+    const keyPad = (number: number[], hand: string) => {
+      const pad = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+        ["*", 0, "#"],
+      ];
+
+      let currL = [3, 0];
+      let currR = [3, 2];
+      let answer: string = "";
+
+      for (let i of number) {
+        if (i === 1 || i === 4 || i === 7) {
+          currL = [(i - 1) / 3, 0];
+          answer += "L";
+        }
+
+        if (i === 3 || i === 6 || i === 9) {
+          currR = [Math.floor((i - 1) / 3), 2];
+          answer += "R";
+        }
+
+        if (i === 2 || i === 5 || i === 8 || i === 0) {
+          let temp: number[] = [];
+          for (let index in pad) {
+            if (pad[index].findIndex((v) => v === i) !== -1) {
+              temp = [
+                parseInt(index, 10),
+                pad[index].findIndex((v) => v === i),
+              ];
+            }
+          }
+
+          const LeftDistance =
+            Math.abs(currL[0] - temp[0]) + Math.abs(currL[1] - temp[1]);
+
+          const RightDistance =
+            Math.abs(currR[0] - temp[0]) + Math.abs(currR[1] - temp[1]);
+
+          if (LeftDistance === RightDistance) {
+            if (hand === "right") {
+              currR = temp;
+              answer += "R";
+            } else {
+              currL = temp;
+              answer += "L";
+            }
+          }
+
+          if (LeftDistance > RightDistance) {
+            currR = temp;
+            answer += "R";
+          }
+
+          if (LeftDistance < RightDistance) {
+            currL = temp;
+            answer += "L";
+          }
+        }
+      }
+
+      return answer;
+    };
+
+    const infomation = (today: any, terms: any, privacies: any) => {
+      // console.log(new Date(today).getFullYear());
+
+      const newTerms = [];
+      const answer = [];
+
+      for (let i of terms) {
+        newTerms.push(i.split(" "));
+      }
+
+      for (let i of privacies) {
+        const tempPerivacies = i.split(" ");
+        let term = 0;
+
+        newTerms.forEach((i) => {
+          if (i[0] === tempPerivacies[1]) term = parseInt(i[1], 10);
+        });
+
+        const calcDate = new Date(tempPerivacies[0]).getMonth() + 1 + term;
+
+        if (calcDate > 12) {
+          // console.log(calcDate);
+        } else {
+        }
+      }
+    };
+
+    // compression(word);
+    // keyPad([1, 2, 3, 4, 5, 6, 7, 8, 9, 0], "right");
+    infomation(
+      "2022.05.19",
+      ["A 6", "B 12", "C 3"],
+      ["2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"]
+    );
   }, []);
-  return (
-    <Box>
-      <Div1></Div1>
-      <Div2></Div2>
-      <Div3></Div3>
-    </Box>
-  );
+  return <></>;
 };
-
-
-const Div1 = styled.div`
-  width: 50%;
-  height: 100px;
-  background-color: red;
-  transition: 0.5s;
-  border-radius: 5px;
-  transform: translateZ(0px) translateY(0px);
-`;
-
-const Div2 = styled.div`
-  width: 50%;
-  height: 100px;
-  background-color: blue;
-  transition: 0.5s;
-  border-radius: 5px;
-  transform: translateZ(30px) translateY(-50px);
-`;
-
-const Div3 = styled.div`
-  width: 50%;
-  height: 100px;
-  background-color: black;
-  transition: 0.5s;
-  border-radius: 5px;
-  transform: translateZ(60px) translateY(-100px);
-`;
-
-const Box = styled.div`
-  width: 500px;
-  height: 600px;
-  background-color: white;
-
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  gap: 10px 0px;
-
-  position: relative;
-  transform-style: preserve-3d;
-  perspective: 500px;
-  grid-area: 0;
-  transition: 0.5s;
-
-  &:hover {
-    div{
-      transform: translateZ(0px) translateY(0px);
-    }
-  }
-`;
 
 export default Test_3;
